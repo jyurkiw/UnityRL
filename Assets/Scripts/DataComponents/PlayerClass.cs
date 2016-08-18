c624798096d96bb1c2e96b0c9a540b53ee419129
jyurkiw@ea.com <jyurkiw@ea.com>
2016-08-17 18:38:28 -0700

    Added a basic GUI to the game.

---
+++ Assets/Scripts/DataComponents/PlayerClass.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerClass : MonoBehaviour {
+	public string _ClassName;
+	public int _Level;
+	public int _MaximumHitPoint;
+	public int _CurrentHitPoints;
+	public int _MaximumMagicPoints;
+	public int _CurrentMagicPoints;
+
+	public bool Dirty = false;
+}
